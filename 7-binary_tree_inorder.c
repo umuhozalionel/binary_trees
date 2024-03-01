457f6b4de95f1ec3f160e84c437e0fd4805831ec
umuhozalionel <muhozalionel@gmail.com>
2024-03-01 07:06:02 -0800

    Pendantic binary tree

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -5,12 +5,12 @@
  * @tree: A pointer to the root node of the tree to traverse.
  * @func: A pointer to a function to call for each node.
  */
-void custom_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL || func == NULL)
-		return;
-
-	custom_tree_inorder(tree->left, func);
-	func(tree->n);
-	custom_tree_inorder(tree->right, func);
+    if (tree && func)
+    {
+        binary_tree_inorder(tree->left, func);
+        func(tree->n);
+        binary_tree_inorder(tree->right, func);
+    }
 }
