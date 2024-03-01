65863020d9303a740ccdc5c3a7ea023a9ba16050
umuhozalionel <muhozalionel@gmail.com>
2024-03-01 03:53:50 -0800

    Binary trees

---
+++ binary_tree_print.c
@@ -0,0 +1,77 @@
+#include "binary_trees.h"
+
+/**
+ * print_tree - Recursively stores each level in an array of strings
+ *
+ * @tree: Pointer to the node to print
+ * @offset: Offset to print
+ * @depth: Depth of the node
+ * @s: Buffer
+ *
+ * Return: Length of printed tree after processing
+ */
+static int print_tree(const binary_tree_t *tree, int offset, int depth, char **s)
+{
+	char b[6];
+	int width, left, right, is_left, i;
+
+	if (!tree)
+		return (0);
+	is_left = (tree->parent && tree->parent->left == tree);
+	width = sprintf(b, "(%03d)", tree->n);
+	left = print_tree(tree->left, offset, depth + 1, s);
+	right = print_tree(tree->right, offset + left + width, depth + 1, s);
+	for (i = 0; i < width; i++)
+		s[depth][offset + left + i] = b[i];
+	if (depth && is_left)
+	{
+		for (i = 0; i < width + right; i++)
+			s[depth - 1][offset + left + width / 2 + i] = '-';
+		s[depth - 1][offset + left + width / 2] = '.';
+	}
+	else if (depth && !is_left)
+	{
+		for (i = 0; i < left + width; i++)
+			s[depth - 1][offset - width / 2 + i] = '-';
+		s[depth - 1][offset + left + width / 2] = '.';
+	}
+	return (left + width + right);
+}
+
+/**
+ * binary_tree_print - Prints a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to print
+ */
+void binary_tree_print(const binary_tree_t *tree)
+{
+	char **s;
+	size_t height, i, j;
+
+	if (!tree)
+		return;
+	height = _height(tree);
+	s = malloc(sizeof(*s) * (height + 1));
+	if (!s)
+		return;
+	for (i = 0; i < height + 1; i++)
+	{
+		s[i] = malloc(sizeof(**s) * 255);
+		if (!s[i])
+			return;
+		memset(s[i], 32, 255);
+	}
+	print_tree(tree, 0, 0, s);
+	for (i = 0; i < height + 1; i++)
+	{
+		for (j = 254; j > 1; --j)
+		{
+			if (s[i][j] != ' ')
+				break;
+			s[i][j] = '\0';
+		}
+		printf("%s\n", s[i]);
+		free(s[i]);
+	}
+	free(s);
+}
