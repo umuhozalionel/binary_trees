e40a5d935b4f0374113e0f7f3202984a28f92949
umuhozalionel <muhozalionel@gmail.com>
2024-03-01 08:32:50 -0800

    Binary trees

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,16 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * trees_inorder - File implementation of in-order traversal.
+ * binary_tree_postorder - Traverses a binary tree using post-order traversal.
  * @tree: A pointer to the root node of the tree to traverse.
  * @func: A pointer to a function to call for each node.
  */
-void trees_inorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree && func)
-	{
-		trees_inorder(tree->left, func);
-		func(tree->n);
-		trees_inorder(tree->right, func);
-	}
+    if (tree && func)
+    {
+        binary_tree_postorder(tree->left, func);
+        binary_tree_postorder(tree->right, func);
+        func(tree->n);
+    }
 }
